ba0ba09b20768e93c2fc09b69f091792042d0a5d
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2023-01-21 14:41:00 -0700

    Create thought model

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Please leave a thought between 1 & 280 characters',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+          getters: true
+        },
+        id: false
+      }
+    );
+    
+    thoughtSchema.virtual('reactionCount').get(function() {
+      return this.reactions.length;
+    });
+
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
