7663280ee1ae7d2c85502c67fb0f4dc1661fa8b3
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2023-01-21 14:32:39 -0700

    Set up user model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type:String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type:String,
+            required: true,
+            unique: true,
+            match: [/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    
+)
