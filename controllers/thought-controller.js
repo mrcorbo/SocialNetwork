84cf4744e327abe5aa83bac90be38459e21cc429
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2023-01-21 17:09:55 -0700

    implemented createThought functionality

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -27,7 +27,24 @@ const thoughtController = {
     },
 
     createThought(req,res) {
-
+        Thought.create(req.body)
+        .then((dbThoughtData) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $push: {thoughts: dbThoughtData._id }},
+                { new: true }
+            )
+        })
+        .then((dbUserData) => {
+            if(!dbUserData) {
+                return res.status(404).json({ message: 'Thought created but no user exists' })
+            }
+            res.json({ message: 'Thought has been thunk' })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json(err)
+        })
     },
     updateThought(req,res) {
 
